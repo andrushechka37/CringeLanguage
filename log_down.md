3d12717008b9596e073f30e87bdf03ed488e6160
andrushechka365 <142117922+andrushechka365@users.noreply.github.com>
2023-12-18 01:34:05 +0300

    minor fix

--- log_down.md
+++ log_down.md
@@ -4,174 +4,172 @@ in get program, call get operator:   ip = 0, type: 4, value: 54
 call another get operator   ip = 0, type: 4, value: 54
 
 
-### in get_operator, call get expression for condition   ip = 3, type: 3, value: 7777
+call another get operator   ip = 1, type: 4, value: 54
 
 
-in get expression, call get subexpression   ip = 3, type: 3, value: 7777
+### in get_operator, call get expression for condition   ip = 4, type: 3, value: 7777
 
 
-in get_subexpression, call get_mul_or_div:   ip = 3, type: 3, value: 7777
+in get expression, call get subexpression   ip = 4, type: 3, value: 7777
 
 
-in get_mul_or_div, call get_pow:   ip = 3, type: 3, value: 7777
+in get_subexpression, call get_mul_or_div:   ip = 4, type: 3, value: 7777
 
 
-in get_pow, call get_bracket:   ip = 3, type: 3, value: 7777
+in get_mul_or_div, call get_pow:   ip = 4, type: 3, value: 7777
 
 
-in get_bracket, call get_long_op:   ip = 3, type: 3, value: 7777
+in get_pow, call get_bracket:   ip = 4, type: 3, value: 7777
 
 
-in get_long_op, call get_variable:   ip = 3, type: 3, value: 7777
+in get_bracket, call get_long_op:   ip = 4, type: 3, value: 7777
 
 
-in get_variable, got variable   ip = 4, type: 4, value: 61
+in get_long_op, call get_variable:   ip = 4, type: 3, value: 7777
 
 
-in get expression, call get subexpression   ip = 5, type: 1, value: 5
+in get_variable, got variable   ip = 5, type: 4, value: 61
 
 
-in get_subexpression, call get_mul_or_div:   ip = 5, type: 1, value: 5
+in get expression, call get subexpression   ip = 6, type: 1, value: 5
 
 
-in get_mul_or_div, call get_pow:   ip = 5, type: 1, value: 5
+in get_subexpression, call get_mul_or_div:   ip = 6, type: 1, value: 5
 
 
-in get_pow, call get_bracket:   ip = 5, type: 1, value: 5
+in get_mul_or_div, call get_pow:   ip = 6, type: 1, value: 5
 
 
-in get_bracket, call get_long_op:   ip = 5, type: 1, value: 5
+in get_pow, call get_bracket:   ip = 6, type: 1, value: 5
 
 
-in get_long_op, call get_variable:   ip = 5, type: 1, value: 5
+in get_bracket, call get_long_op:   ip = 6, type: 1, value: 5
 
 
-in get_variable, call get_number:   ip = 5, type: 1, value: 5
+in get_long_op, call get_variable:   ip = 6, type: 1, value: 5
 
 
-in get_number   ip = 5, type: 1, value: 5
+in get_variable, call get_number:   ip = 6, type: 1, value: 5
 
 
-### in get_operator, call get operator for body   ip = 8, type: 1, value: 7
+in get_number   ip = 6, type: 1, value: 5
 
 
-### int get operator call get expression   ip = 8, type: 1, value: 7
+### in get_operator, call get operator for body   ip = 9, type: 1, value: 7
 
 
-in get expression, call get subexpression   ip = 8, type: 1, value: 7
+### int get operator call get expression   ip = 9, type: 1, value: 7
 
 
-in get_subexpression, call get_mul_or_div:   ip = 8, type: 1, value: 7
+in get expression, call get subexpression   ip = 9, type: 1, value: 7
 
 
-in get_mul_or_div, call get_pow:   ip = 8, type: 1, value: 7
+in get_subexpression, call get_mul_or_div:   ip = 9, type: 1, value: 7
 
 
-in get_pow, call get_bracket:   ip = 8, type: 1, value: 7
+in get_mul_or_div, call get_pow:   ip = 9, type: 1, value: 7
 
 
-in get_bracket, call get_long_op:   ip = 8, type: 1, value: 7
+in get_pow, call get_bracket:   ip = 9, type: 1, value: 7
 
 
-in get_long_op, call get_variable:   ip = 8, type: 1, value: 7
+in get_bracket, call get_long_op:   ip = 9, type: 1, value: 7
 
 
-in get_variable, call get_number:   ip = 8, type: 1, value: 7
+in get_long_op, call get_variable:   ip = 9, type: 1, value: 7
 
 
-in get_number   ip = 8, type: 1, value: 7
+in get_variable, call get_number:   ip = 9, type: 1, value: 7
 
 
-in get expression, call get subexpression   ip = 10, type: 1, value: 5
+in get_number   ip = 9, type: 1, value: 7
 
 
-in get_subexpression, call get_mul_or_div:   ip = 10, type: 1, value: 5
+in get expression, call get subexpression   ip = 11, type: 1, value: 5
 
 
-in get_mul_or_div, call get_pow:   ip = 10, type: 1, value: 5
+in get_subexpression, call get_mul_or_div:   ip = 11, type: 1, value: 5
 
 
-in get_pow, call get_bracket:   ip = 10, type: 1, value: 5
+in get_mul_or_div, call get_pow:   ip = 11, type: 1, value: 5
 
 
-in get_bracket, call get_long_op:   ip = 10, type: 1, value: 5
+in get_pow, call get_bracket:   ip = 11, type: 1, value: 5
 
 
-in get_long_op, call get_variable:   ip = 10, type: 1, value: 5
+in get_bracket, call get_long_op:   ip = 11, type: 1, value: 5
 
 
-in get_variable, call get_number:   ip = 10, type: 1, value: 5
+in get_long_op, call get_variable:   ip = 11, type: 1, value: 5
 
 
-in get_number   ip = 10, type: 1, value: 5
+in get_variable, call get_number:   ip = 11, type: 1, value: 5
 
 
-### 0x103900850 is value ptr 0x103900890
-blablka   ip = 12, type: 1, value: 4
+in get_number   ip = 11, type: 1, value: 5
 
 
-### int get operator call get expression   ip = 12, type: 1, value: 4
+### int get operator call get expression   ip = 13, type: 1, value: 4
 
 
-in get expression, call get subexpression   ip = 12, type: 1, value: 4
+in get expression, call get subexpression   ip = 13, type: 1, value: 4
 
 
-in get_subexpression, call get_mul_or_div:   ip = 12, type: 1, value: 4
+in get_subexpression, call get_mul_or_div:   ip = 13, type: 1, value: 4
 
 
-in get_mul_or_div, call get_pow:   ip = 12, type: 1, value: 4
+in get_mul_or_div, call get_pow:   ip = 13, type: 1, value: 4
 
 
-in get_pow, call get_bracket:   ip = 12, type: 1, value: 4
+in get_pow, call get_bracket:   ip = 13, type: 1, value: 4
 
 
-in get_bracket, call get_long_op:   ip = 12, type: 1, value: 4
+in get_bracket, call get_long_op:   ip = 13, type: 1, value: 4
 
 
-in get_long_op, call get_variable:   ip = 12, type: 1, value: 4
+in get_long_op, call get_variable:   ip = 13, type: 1, value: 4
 
 
-in get_variable, call get_number:   ip = 12, type: 1, value: 4
+in get_variable, call get_number:   ip = 13, type: 1, value: 4
 
 
-in get_number   ip = 12, type: 1, value: 4
+in get_number   ip = 13, type: 1, value: 4
 
 
-in get expression, call get subexpression   ip = 14, type: 1, value: 6
+in get expression, call get subexpression   ip = 15, type: 1, value: 6
 
 
-in get_subexpression, call get_mul_or_div:   ip = 14, type: 1, value: 6
+in get_subexpression, call get_mul_or_div:   ip = 15, type: 1, value: 6
 
 
-in get_mul_or_div, call get_pow:   ip = 14, type: 1, value: 6
+in get_mul_or_div, call get_pow:   ip = 15, type: 1, value: 6
 
 
-in get_pow, call get_bracket:   ip = 14, type: 1, value: 6
+in get_pow, call get_bracket:   ip = 15, type: 1, value: 6
 
 
-in get_bracket, call get_long_op:   ip = 14, type: 1, value: 6
+in get_bracket, call get_long_op:   ip = 15, type: 1, value: 6
 
 
-in get_long_op, call get_variable:   ip = 14, type: 1, value: 6
+in get_long_op, call get_variable:   ip = 15, type: 1, value: 6
 
 
-in get_variable, call get_number:   ip = 14, type: 1, value: 6
+in get_variable, call get_number:   ip = 15, type: 1, value: 6
 
 
-in get_number   ip = 14, type: 1, value: 6
+in get_number   ip = 15, type: 1, value: 6
 
 
-### 0x103900750 is value ptr 0x103900790
-blablka   ip = 16, type: 4, value: 53
+#### while ends   ip = 17, type: 4, value: 53
 
 
-#### while ends   ip = 16, type: 4, value: 53
+#### while ends   ip = 17, type: 4, value: 53
 
 
-### 0x1039006d0 is value ptr 0x103900710
-blablka   ip = 16, type: 4, value: 53
+get_operator 231-line
+## error with bracket   ip = 18, type: 4, value: 54
 
 
-#### while ends   ip = 16, type: 4, value: 53
+## end of program is wrong   ip = 19, type: 1, value: 5
 
 
--- recursive_down.cpp
+++ recursive_down.cpp
@@ -14,13 +14,13 @@
     fprintf(plog, text);     \
     PRINT_CUR_SIT;
 
-#define CHECK_BRACKET(name)                      \
-    if (IS_ELEM(syntax_t, name)) {               \
-        token_num++;                             \
-    } else {                                     \
+#define CHECK_BRACKET(name)                                        \
+    if (IS_ELEM(syntax_t, name)) {                                 \
+        token_num++;                                               \
+    } else {                                                       \
         fprintf(plog, "%s %d-line", __FUNCTION__, __LINE__);       \
-        PRINT_REPORT("\n## error with bracket"); \
-        token_num++;                             \
+        PRINT_REPORT("\n## error with bracket");                   \
+        token_num++;                                               \
     }
 
 static diff_tree_element * get_subexpression(token_array * parsed_program);
@@ -40,10 +40,13 @@ static diff_tree_element * get_number(token_array * parsed_program) {
 static diff_tree_element * get_variable(token_array * parsed_program) {
     if (TYPE_OF_TOKEN == variable_t) {
         token_num++;
+
         PRINT_REPORT("in get_variable, got variable");
+
         return VAR;
     } else {
         PRINT_REPORT("in get_variable, call get_number:");
+
         return get_number(parsed_program);
     }
 }
@@ -61,11 +64,16 @@ static diff_tree_element * get_variable(token_array * parsed_program) {
 
 static diff_tree_element * get_long_op(token_array * parsed_program) {
     if (IS_ELEM(operator_t, OP_SIN)) {
+
         CREATE_LONG_OP_NODE(SIN(get_subexpression(parsed_program)));
+
     } else if (IS_ELEM(operator_t, OP_COS)) {
+
         CREATE_LONG_OP_NODE(COS(get_subexpression(parsed_program)));
+
     } else {
         PRINT_REPORT("in get_long_op, call get_variable:");
+
         return get_variable(parsed_program);
     }
 }
@@ -77,13 +85,14 @@ static diff_tree_element * get_bracket(token_array * parsed_program) {
         CHECK_BRACKET(OP_ROUND_O)
 
         PRINT_REPORT("in get_bracket, call get_subexpression:");
-        diff_tree_element * value = get_subexpression(parsed_program);
 
+        diff_tree_element * value = get_subexpression(parsed_program);
         CHECK_BRACKET(OP_ROUND_C);
 
         return value;
     } else {
         PRINT_REPORT("in get_bracket, call get_long_op:");
+
         return get_long_op(parsed_program);
     }
 }
@@ -109,6 +118,7 @@ static diff_tree_element * get_mul_or_div(token_array * parsed_program) {
     PRINT_REPORT("in get_mul_or_div, call get_pow:");
  
     diff_tree_element * value = get_pow(parsed_program);
+
     while (IS_ELEM(operator_t, OP_MUL) || IS_ELEM(operator_t, OP_DIV)) {
 
         int op = VALUE_OF_TOKEN;
@@ -117,16 +127,17 @@ static diff_tree_element * get_mul_or_div(token_array * parsed_program) {
         PRINT_REPORT("in get_mul_or_div, call get_pow:");
 
         diff_tree_element * value2 = get_pow(parsed_program);
+
         switch (op) {
-        case OP_MUL:
-            value =  MUL(value, value2);
-            break;
-        case OP_DIV:
-            value = DIV(value, value2);
-            break;
-        default:
-            PRINT_REPORT("## not * or / in get_mul_or_div");
-            break;
+            case OP_MUL:
+                value =  MUL(value, value2);
+                break;
+            case OP_DIV:
+                value = DIV(value, value2);
+                break;
+            default:
+                PRINT_REPORT("## not * or / in get_mul_or_div");
+                break;
         }
     }
     return value;
@@ -136,6 +147,7 @@ static diff_tree_element * get_subexpression(token_array * parsed_program) {
     PRINT_REPORT("in get_subexpression, call get_mul_or_div:");
 
     diff_tree_element * value = get_mul_or_div(parsed_program);
+
     while (IS_ELEM(operator_t, OP_ADD) || IS_ELEM(operator_t, OP_SUB)) {
         int op = VALUE_OF_TOKEN;
         token_num++; // skip op
@@ -144,15 +156,15 @@ static diff_tree_element * get_subexpression(token_array * parsed_program) {
 
         diff_tree_element * value2 = get_mul_or_div(parsed_program);
         switch (op) {
-        case OP_ADD:
-            value = ADD(value, value2);
-            break;
-        case OP_SUB:
-            value = SUB(value, value2);
-            break;
-        default:
-            PRINT_REPORT("## not + or - in get_subexpression");
-            break;
+            case OP_ADD:
+                value = ADD(value, value2);
+                break;
+            case OP_SUB:
+                value = SUB(value, value2);
+                break;
+            default:
+                PRINT_REPORT("## not + or - in get_subexpression");
+                break;
         }
     }
     return value;
@@ -169,9 +181,12 @@ static diff_tree_element * get_subexpression(token_array * parsed_program) {
 diff_tree_element * get_expression(token_array * parsed_program) {
     PRINT_REPORT("in get expression, call get subexpression");
 
-    diff_tree_element * value = get_subexpression(parsed_program); // is not ;
+    diff_tree_element * value = get_subexpression(parsed_program);
+
     if (!IS_ELEM(syntax_t, OP_END)) {
+
         diff_tree_element * right = NULL;
+
         switch ((operations)VALUE_OF_TOKEN) {
             case OP_LESS:
                 CREATE_EXPRESSION_NODE(LESS(value, right));
@@ -184,7 +199,7 @@ diff_tree_element * get_expression(token_array * parsed_program) {
                 break;
         }
     } else {
-        CHECK_BRACKET(OP_END);
+        CHECK_BRACKET(OP_END);             
         return value;
     }
 
@@ -192,23 +207,25 @@ diff_tree_element * get_expression(token_array * parsed_program) {
 
 #undef CREATE_EXPRESSION_NODE
 
-#define CREATE_OP_NODE(func)                                        \
-    token_num++;                                                    \
-    CHECK_BRACKET(OP_ROUND_O);                                      \
-                                                                    \
-    PRINT_REPORT("### in get_operator, call get expression for condition");           \
-    diff_tree_element * condition = get_expression(parsed_program); \
-    CHECK_BRACKET(OP_ROUND_C);                                      \
-                                                                    \
+#define CREATE_OP_NODE(func)                                                     \
+    token_num++;                                                                 \
+    CHECK_BRACKET(OP_ROUND_O);                                                   \
+                                                                                 \
+    PRINT_REPORT("### in get_operator, call get expression for condition");      \
+    diff_tree_element * condition = get_expression(parsed_program);              \
+    CHECK_BRACKET(OP_ROUND_C);                                                   \
+                                                                                 \
     PRINT_REPORT("### in get_operator, call get operator for body");             \
-    diff_tree_element * body = get_operator(parsed_program);        \
-                                                                    \
+    diff_tree_element * body = get_operator(parsed_program);                     \
+                                                                                 \
     cur_node = func;
 
 diff_tree_element * get_operator(token_array * parsed_program) {
 
     if (IS_ELEM(syntax_t, OP_FIG_O)) {
+
         PRINT_REPORT("call another get operator");
+
         CHECK_BRACKET(OP_FIG_O);
         diff_tree_element * value = get_operator(parsed_program);
         CHECK_BRACKET(OP_FIG_C);
@@ -217,9 +234,9 @@ diff_tree_element * get_operator(token_array * parsed_program) {
     } else {
         diff_tree_element * value = NULL;
         diff_tree_element * cur_node = NULL;
-        diff_tree_element * prev_node = NULL;
+
         while (!IS_ELEM(syntax_t, OP_FIG_C)) {
-            if (IS_ELEM(syntax_t, OP_IF)) { // value перезаписывается 
+            if (IS_ELEM(syntax_t, OP_IF)) { 
 
                 CREATE_OP_NODE(IF(condition, body));
 
@@ -231,10 +248,8 @@ diff_tree_element * get_operator(token_array * parsed_program) {
                 PRINT_REPORT("### int get operator call get expression")
                 cur_node = get_expression(parsed_program);
             }
-            prev_node = value;
-            value = node_ctor(OP_END, syntax_t, cur_node, prev_node, NULL);
-            fprintf(plog, "### %p is value ptr %p\n", value, value->left);
-            PRINT_REPORT("blablka");
+            
+            value = node_ctor(OP_END, syntax_t, cur_node, value, NULL);
         }
         PRINT_REPORT("#### while ends");
         return value;
