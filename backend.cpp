3d6817aeb6cd0b9b8f30585528e395fb2642dcc2
andrushechka365 <142117922+andrushechka365@users.noreply.github.com>
2024-01-03 22:32:34 +0300

    multiple variables in backend

--- backend.cpp
+++ backend.cpp
@@ -12,17 +12,15 @@
 #include <string.h>
 
 static bool check_symbol(char symbol, FILE * pfile);
+static int set_variable(char name[]);
+variables_info variables_table;
 
 void set_type_value(diff_tree_element * element, double number, types_of_node type) {
 
     if (type == variable_t || type == value_t) {
-
         element->value.number = number;
-
     } else {
-
         element->value.operator_info.op_number = (operations)number;
-
     }
 
     element->type = type;
@@ -58,9 +56,10 @@ int build_tree(elem_ptr * element, FILE * in_file, elem_ptr * parent) {
                 SET_RIGHT_TYPE_VALUE(is_func_name(op));
 
             } else {
+                int num = set_variable(op);
 
-                set_type_value(*element, 1, variable_t);
-                
+                set_type_value(*element, num, variable_t);
+
             }
         }
 
@@ -92,7 +91,7 @@ diff_tree_element * read_tree() {
 }
 
 int main(void) {
-
+    variables_table.size = 0;
     diff_tree_element * tree = read_tree();
 
     tree_visualize(tree);
@@ -119,7 +118,30 @@ static bool check_symbol(char symbol, FILE * pfile) {
     return is_found;
 }
 
+#define VAR_NUM (variables_table.size)
 
+static int set_variable(char name[]) {
+
+    int i = 0;
+    while (i < variables_table.size) {
+
+        if (strcmp(name, variables_table.table[i].name) == 0) {
+            return i;
+        }
+        i++;
+    }
+
+    strcpy(variables_table.table[VAR_NUM].name, name); // not safe
+    variables_table.table[VAR_NUM].value = VAR_NUM;
+
+    VAR_NUM++;
+    
+    if (VAR_NUM > VARIABLE_COUNT) {
+        printf("!!!!!!!!!!!!!!!!!!too much variables");
+    }
+    
+    return VAR_NUM - 1;
+}
 
 
 
